0f7e359662fdb0ecffc9d0e882eb9cd9f06c61d8
Luis <m.luishernandez@outlook.com>
2021-07-20 22:55:11 -0400

    Added function that renders searched weather forecast

--- public/script.js
+++ public/script.js
@@ -8,20 +8,29 @@ $(document).ready(function () {
         url:"http://api.openweathermap.org/data/2.5/weather?q=Kissimmee&appid=" + currentWeatherKey,
         method:"GET"
     }).then(function (response) {
-        console.log(response);
+        // console.log(response);
 
         //OnClick search button
         $("#search-button").on("click", function () {
             //city will get the value from the search value form
             var cityValue = $("#search-value").val();
-            // console.log(cityValue);
             var listElement = $("<li>").addClass("list-group-item list-group-item-action");
-            var complete = $(listElement).appendTo(".history").text(cityValue)
-            console.log(complete);
+             $(listElement).appendTo(".history").text(cityValue);
+            searchedWeather()
         });
-        
-    });
 
+
+        function searchedWeather() {
+            console.log(response);
+            var h1El = $("<h1>").text(response.name);
+            var tempPtag = $("<p>").text("Temperature: " + response.main.temp + " °F");
+            var humidityPtag = $("<p>").text("Humidity: " + response.main.humidity + "%");
+            var windPtag = $("<p>").text("Wind Speed: " + response.wind.speed + "MPH");
+            //UV index need to be extracted from another API call
+            // var uvIndexpTag = $("<p>").text()
+            $("#today").append(h1El, tempPtag, humidityPtag, windPtag).addClass("border");
+        }
+    });
 
     
 })
